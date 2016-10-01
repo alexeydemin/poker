84240f714338dc1a1e39fbaed85cd4a1f499b764
Alexey Demin <deminn@gmail.com>
2016-10-01 18:49:29 -0300

    Initial commit

---
+++ app/Set.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App;
+
+class Set {
+    const NUMBER = 'number';
+    const SUIT = 'suit';
+    public $set;
+    private $n_pics = ['A' => '1', '2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '6'=>'6', '7'=>'7', '8' => '8', '9' => '9',
+        '10' => 'a', 'J' => 'b', 'Q' => 'd', 'K' => 'e' ];
+    private $s_pics = ['spades' => 'a', 'hearts' => 'b', 'diamonds' => 'c', 'clubs' => 'd'];
+
+
+    public function __construct($json)
+    {
+        $this->suit_str = self::SUIT;
+        $this->number_str = self::NUMBER;
+        $this->set = json_decode($json);
+        $this->addPics();
+        if( $this->set  != array_unique($this->set, SORT_REGULAR) ){
+            throw new Exception('Deck inconsistency detected!');
+        }
+    }
+
+    private function addPics()
+    {
+        $ss = $this->suit_str;
+        $ns = $this->number_str;
+        foreach($this->set as $s){
+            $s->unicode = '&#x1f0' . $this->s_pics[ $s->$ss ] . $this->n_pics[ $s->$ns ] ;
+        }
+        print_r( $this->set);
+    }
+
+    public function getSuits()
+    {
+
+        $res = [];
+        foreach( $this->set as $card ) {
+            $val = $this->suit_str;
+            $res[] = $card->$val;
+            if( !in_array($card->$val, Combination::$suits) ){
+                throw new Exception('Unknown card suit');
+            }
+        }
+        return $res;
+    }
+
+    public function getNumbers()
+    {
+        $res = [];
+        foreach( $this->set as $card ) {
+            $val = $this->number_str;
+            $res[] = $card->$val;
+            if( !in_array($card->$val, Combination::$order) ){
+                throw new Exception('Unknown card number');
+            }
+        }
+
+        return $res;
+    }
+}
